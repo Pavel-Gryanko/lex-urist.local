955eb9c3c23bf2db85383a999d0c0489b2ad1534
Pavel Gryanko <pavelgruanko23@gmail.com>
2021-02-12 06:15:24 +0300

    fix-work-site

---
+++ wp-content/themes/lex-urist/cpt-alm/loop/reviews-list.php
@@ -0,0 +1,7 @@
+<article class="reviews-col">
+  <div class="reviews-author-face" style="background-image: url('<?php the_post_thumbnail_url('full'); ?>');"></div>
+  <div class="reviews-author-data"><span class="reviews-author__name"><?php the_title(); ?></span>
+    <address class="reviews-author__adress"><?php the_field('reviewsCity'); ?></address>
+  </div>
+  <div><i><?php the_excerpt(); ?></i></div>
+</article>
